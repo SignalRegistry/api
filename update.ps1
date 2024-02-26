2a533dde83ab6b43b6e6f4a3ebeccb2e29f8f66c
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2024-02-26 22:13:33 +0300

    Update script

--- update.ps1
+++ update.ps1
@@ -1,1 +1,3 @@
-ssh root@167.71.34.169 "cd ~/SignalRegistry/api && git pull && npm i && pm2 restart api"
+ssh root@142.93.172.57 "cd ~/SignalRegistry/api && git checkout node.js && git pull"
+ssh root@142.93.172.57 "cd ~/SignalRegistry/api && bash bootstrap.sh"
+ssh root@142.93.172.57 "cd ~/SignalRegistry/api && npm i && pm2 restart api"
