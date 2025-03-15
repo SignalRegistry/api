ab3cece95a48f2863d282928ff8ea4dcefa34434
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2025-03-15 23:14:07 +0300

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
   "deploy": {
     "production": {
       "user": "root",
-      "host": "164.92.135.26",
+      "host": "localhost",
       "ref": "origin/main",
       "repo": "https://github.com/SignalRegistry/api.git",
       "path": "/root/api",
