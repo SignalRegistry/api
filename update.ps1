a0f5027205d6013fa84c0062ff64d3531598b2b0
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2025-03-18 00:13:17 +0300

    name conventions

--- update.ps1
+++ update.ps1
@@ -1,3 +1,3 @@
 ssh root@164.92.135.26 "cd ~/SignalRegistry/api && git pull"
 ssh root@164.92.135.26 "cd ~/SignalRegistry/api && bash bootstrap.sh"
-ssh root@164.92.135.26 "cd ~/SignalRegistry/api && npm i && pm2 restart api"
+ssh root@164.92.135.26 "cd ~/SignalRegistry/api && npm i && pm2 restart api"1
