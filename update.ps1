4fe7169aa9d218adb5735b94616103c4a49e4741
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2025-03-17 22:39:49 +0300

    update ip address

--- update.ps1
+++ update.ps1
@@ -1,3 +1,3 @@
-ssh root@142.93.172.57 "cd ~/SignalRegistry/api && git checkout node.js && git pull"
-ssh root@142.93.172.57 "cd ~/SignalRegistry/api && bash bootstrap.sh"
-ssh root@142.93.172.57 "cd ~/SignalRegistry/api && npm i && pm2 restart api"
+ssh root@164.92.135.26 "cd ~/SignalRegistry/api && git pull"
+ssh root@164.92.135.26 "cd ~/SignalRegistry/api && bash bootstrap.sh"
+ssh root@164.92.135.26 "cd ~/SignalRegistry/api && npm i && pm2 restart api"
