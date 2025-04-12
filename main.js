d68e2f27df615d002102746342be5a577211a94f
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2025-04-12 17:48:34 +0300

    Old plain app reinitiated

--- main.js
+++ main.js
@@ -8,7 +8,48 @@ const assert    = require("assert")
 const crypto    = require('crypto')
 const { spawn } = require('node:child_process');
 
-const { mongoClient, mongoOff } = require("./database/mongodb.js");
+// =============================================================================
+// MongoDB Client
+// =============================================================================
+const { MongoClient, ObjectId } = require("mongodb");
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const mongoClient = new MongoClient(`mongodb://${process.env.MONGODB_SERVER || "127.0.0.1"}:27017`, {
+  // connectTimeoutMS: 1000,
+  serverSelectionTimeoutMS: 5000,
+  minPoolSize: 10,
+  maxPoolSize: 20
+});
+
+let mongoOff = true
+mongoClient.on('serverHeartbeatFailed', event => {
+  if (!mongoOff) console.log("[ERROR] Database connection is offline.");
+  mongoOff = true
+});
+mongoClient.on('serverHeartbeatSucceeded', event => {
+  if (mongoOff) console.log("[INFO] Database connection is online.");
+  mongoOff = false
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await mongoClient.connect();
+    mongoOff = false
+
+    // Send a ping to confirm a successful connection
+    await mongoClient.db("admin").command({ ping: 1 });
+
+    console.log(`[INFO] Successfully connected to MongoDB at ${process.env.MONGODB_SERVER || "127.0.0.1"}`);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoClient.close();
+  }
+}
+run().catch(() => {
+  console.log("[INFO] Could not connected to MongoDB!");
+});
 
 
 
@@ -28,7 +69,7 @@ server.listen(port, () => {
 let morgan = require('morgan')
 morgan.token('session', function (req, res) { return req.session || req.token || 'no-session'.padEnd(32, '#') })
 morgan.token('username', function (req, res) { return  (req.user && req.user['username']) ? req.user['username'].padEnd(16, '_') : 'no-user'.padEnd(16, '#') })
-app.use(morgan('[LOG] :method :status :response-time :req[content-length] :res[content-length] :session :username :url'))
+app.use(morgan('[LOG] :remote-addr :method :status :response-time :req[content-length] :res[content-length] :session :username :url'))
 
 app.use(require('express').urlencoded({ extended: true }));
 app.use(require('cookie-parser')())
